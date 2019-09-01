7af9066ae64bb1abb54e61fb1a731f65901c4690
Jakub Hermyt <kubahermyt@gmail.com>
2019-09-01 13:46:25 +0200

    feat: used fetch api

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
-"use strict";
+const repoList = document.querySelector(`.projects__list--js`);
 
-// place your code below
+fetch(`https://api.github.com/users/JakHer/repos?sort=updated`)
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      let {
+        name,
+        html_url,
+        description,
+        homepage
+      } = repo;
+      repoList.innerHTML +=
+        `<li class="projects__item">
+        <div class="projects__container">
+        <img src="../assets/img/githubIcon.svg" alt="github icon.">
+        <h3 class="projects__heading">${name}</h3>
+        <p class="projects__paragraph">${description ? description : ''}</p>
+        </div>
 
-
-console.log(`Hello world!`);
-
-
+        <div class="projects__links">
+    ${homepage ? `<a class="projects__demo" href="${homepage}" target="_blank rel=" nofollow noreferrer"" > Demo</a >` : ''
+        }
+    <a class="projects__demo" href="${html_url}" target="_blank rel=" nofollow noreferrer"" > GitHub</a >
+        </div >
+      </li > `
+      console.log(html_url)
+    }
+  })
