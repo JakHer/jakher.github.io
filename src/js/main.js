a8f70835cea8ec3b68d5a7bf2f0ced6c9566b9c5
Jakub Hermyt <kubahermyt@gmail.com>
2019-11-13 22:26:56 +0100

    feat: api url to const

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,30 @@
 const repoList = document.querySelector(`.projects__list--js`);
-fetch(`https://api.github.com/users/JakHer/repos?sort=updated`)
+const baseURL = `https://api.github.com/users/JakHer/repos?sort=updated`;
+
+fetch(baseURL)
   .then(resp => resp.json())
   .then(resp => {
     const repos = resp;
     for (const repo of repos) {
-      let {
-        name,
-        html_url,
-        description,
-        homepage
-      } = repo;
-      repoList.innerHTML +=
-        `<li class="projects__item">
+      let { name, html_url, description, homepage } = repo;
+      repoList.innerHTML += `<li class="projects__item">
         <div class="projects__container">
         <img class="projects__img" src="../assets/img/githubIcon.svg" alt="github icon.">
         <h3 class="projects__heading">${name}</h3>
-          ${description ? ` <p class="projects__paragraph">${description}</p>` : ''}
+          ${
+            description
+              ? ` <p class="projects__paragraph">${description}</p>`
+              : ""
+          }
         </div>
         <div class="projects__links">
-    ${homepage ? `<a class="projects__button projects__button--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a >` : ''
-        }
+    ${
+      homepage
+        ? `<a class="projects__button projects__button--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a >`
+        : ""
+    }
     <a class="projects__button projects__button--github" href="${html_url}" target="_blank" rel="nofollow noreferrer" >GitHub</a >
         </div >
-      </li > `
+      </li > `;
     }
-  })
+  });
