1aae76a25c5148fd6eb3afd6b40dd6cdacbb7940
Jakub Hermyt <kubahermyt@gmail.com>
2019-11-13 22:50:56 +0100

    feat: fetch api refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,42 @@
 const repoList = document.querySelector(`.projects__list--js`);
-const baseURL = `https://api.github.com/users/JakHer/repos?sort=updated`;
+const baseURL = `https://api.github.com/users/JakHer/repos?sort=`;
+const sort = `updated`;
 
-fetch(baseURL)
-  .then(resp => resp.json())
-  .then(resp => {
-    const repos = resp;
-    for (const repo of repos) {
-      let { name, html_url, description, homepage } = repo;
-      repoList.innerHTML += `<li class="projects__item">
-        <div class="projects__container">
-        <img class="projects__img" src="../assets/img/githubIcon.svg" alt="github icon.">
-        <h3 class="projects__heading">${name}</h3>
-          ${
-            description
-              ? ` <p class="projects__paragraph">${description}</p>`
-              : ""
-          }
-        </div>
-        <div class="projects__links">
-    ${
-      homepage
-        ? `<a class="projects__button projects__button--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a >`
-        : ""
-    }
-    <a class="projects__button projects__button--github" href="${html_url}" target="_blank" rel="nofollow noreferrer" >GitHub</a >
-        </div >
-      </li > `;
-    }
-  });
+const getRepo = sort => {
+  return fetch(`${baseURL}${sort}`)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => error);
+};
+
+getRepo(sort).then(data => {
+  const [repo] = data;
+  const { name, description } = repo;
+  const repoList = `Nazwa: ${name}, opis: ${description} `;
+  console.log(repoList);
+});
+
+// const repos = resp;
+// for (const repo of repos) {
+//   let { name, html_url, description, homepage } = repo;
+//   repoList.innerHTML += `<li class="projects__item">
+//         <div class="projects__container">
+//         <img class="projects__img" src="../assets/img/githubIcon.svg" alt="github icon.">
+//         <h3 class="projects__heading">${name}</h3>
+//           ${
+//             description
+//               ? ` <p class="projects__paragraph">${description}</p>`
+//               : ""
+//           }
+//         </div>
+//         <div class="projects__links">
+//     ${
+//       homepage
+//         ? `<a class="projects__button projects__button--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a >`
+//         : ""
+//     }
+//     <a class="projects__button projects__button--github" href="${html_url}" target="_blank" rel="nofollow noreferrer" >GitHub</a >
+//         </div >
+//       </li > `;
+// }
+//
