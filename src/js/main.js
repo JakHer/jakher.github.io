da332115af4afb7e82cdf447289bd096b16fd722
Jakub Hermyt <kubahermyt@gmail.com>
2019-11-14 11:34:02 +0100

    feat: fetch api refactored

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,67 @@ const repoList = document.querySelector(`.projects__list--js`);
 const baseURL = `https://api.github.com/users/JakHer/repos?sort=`;
 const sort = `updated`;
 
-const getRepo = sort => {
-  return fetch(`${baseURL}${sort}`)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => error);
+const getRepo = async sort => {
+  try {
+    const response = await fetch(`${baseURL}${sort}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 getRepo(sort).then(data => {
-  const [repo] = data;
-  const { name, description } = repo;
-  const repoList = `Nazwa: ${name}, opis: ${description} `;
-  console.log(repoList);
+  for (let repo of data) {
+    const { name, html_url, description, homepage } = repo;
+
+    const li = document.createElement(`li`);
+    li.classList.add(`projects__item`);
+
+    const div = document.createElement(`div`);
+    div.classList.add(`projects__container`);
+
+    const img = document.createElement(`img`);
+    img.classList.add(`projects__img`);
+    img.setAttribute(`src`, `../assets/img/githubIcon.svg`);
+    img.setAttribute(`alt`, `Github icon.`);
+
+    const h3 = document.createElement(`h3`);
+    h3.classList.add(`projects__heading`);
+    h3.textContent = `${name}`;
+
+    const p = document.createElement(`p`);
+    p.classList.add(`projects__paragraph`);
+    p.textContent = `${description}`;
+
+    const linkDiv = document.createElement(`div`);
+    linkDiv.classList.add(`projects__links`);
+
+    const demo = document.createElement(`a`);
+    demo.classList.add(`projects__button`, `projects__button--demo`);
+    demo.setAttribute(`href`, `${homepage}`);
+    demo.setAttribute(`target`, `_blank`);
+    demo.setAttribute(`rel`, `nofollow noreferrer`);
+    demo.textContent = `Demo`;
+
+    const github = document.createElement(`a`);
+    github.classList.add(`projects__button`, `projects__button--github`);
+    github.setAttribute(`href`, `${html_url}`);
+    github.setAttribute(`target`, `_blank`);
+    github.setAttribute(`rel`, `nofollow noreferrer`);
+    github.textContent = `Github`;
+
+    repoList.appendChild(li);
+    li.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(h3);
+    li.appendChild(linkDiv);
+
+    description ? div.appendChild(p) : ``;
+
+    homepage ? linkDiv.appendChild(demo) : ``;
+    html_url ? linkDiv.appendChild(github) : ``;
+  }
 });
 
 // const repos = resp;
